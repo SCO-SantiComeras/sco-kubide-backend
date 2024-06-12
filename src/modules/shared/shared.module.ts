2a7da5801ea74ee058bba3467b5d03a086aed675
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-06-12 22:50:35 +0200

    ADD - Swagger Controller Service

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { BcryptService } from './bcrypt/bcrypt.service';
+import { ControllerService } from './controller/controller.service';
 
 @Module({
   providers: [
     BcryptService,
+    ControllerService,
   ],
   exports: [
     BcryptService,
+    ControllerService,
   ],
 })
 export class SharedModule {}
