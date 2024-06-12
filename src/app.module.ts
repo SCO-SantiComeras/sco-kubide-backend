5af59b828224919dac9d92fbb8916dac6052cfa4
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-06-12 20:01:00 +0200

    ADD - MongoDB module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongoDbModule } from './modules/mongo-db/mongo-db.module';
 
 @Module({
   imports: [
-
+    MongoDbModule.register({
+      ip: '127.0.0.1',
+      port: 27017,
+      database: 'sco-kubide-backend',
+    })
   ],
   providers: [
 
---
+++ src/modules/mongo-db/mongo-db.constants.ts
@@ -0,0 +1,6 @@
+export const MONGODB_CONSTANTS = {
+    USERS: {
+        MODEL: 'User',
+        TABLE: 'users',
+    },
+}
