f906296a50fbb40672cd8c007e525e43981176d1
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-06-12 20:15:19 +0200

    UPD - Mongodb Module Import Async Mode

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongoDbModule } from './modules/mongo-db/mongo-db.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { configurationApp } from './configuration/configuration-app';
 import { configurationMongo } from './configuration/configuration-mongo';
+import { MongoDbConfig } from './modules/mongo-db/mongo-db-config';
 
 @Module({
   imports: [
@@ -14,11 +15,20 @@ import { configurationMongo } from './configuration/configuration-mongo';
       envFilePath: `./env/${process.env.NODE_ENV}.env`,
       isGlobal: true,
     }),
-    MongoDbModule.register({
-      ip: '127.0.0.1',
-      port: 27017,
-      database: 'sco-kubide-backend',
-    })
+    MongoDbModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const mongoDB: MongoDbConfig = {
+          ip: configService.get('mongo.host'),
+          port: configService.get('mongo.port'),
+          database: configService.get('mongo.database'),
+          user: configService.get('mongo.user'),
+          pass: configService.get('mongo.password'),
+        };
+        return mongoDB;
+      },
+      inject: [ConfigService],
+    }),
   ],
   providers: [
 
