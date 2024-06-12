028f5d317054e404bd15332f886982eb6028540a
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-06-12 20:44:04 +0200

    ADD - Users HTTP Error constants

---
+++ src/constants/http-error-messages.constants.ts
@@ -0,0 +1,10 @@
+export const HTTP_ERROR_CONSTANTS = {
+  USERS: {
+    USER_NOT_FOUND: 'User not found',
+    USER_ALREADY_EXIST: 'User already exist',
+    EMAIL_ALREADY_EXIST: 'Email already registered',
+    CREATE_USER_ERROR: 'Unnable to create user',
+    UPDATE_USER_ERROR: 'Unnable to update user',
+  },
+};
+  
