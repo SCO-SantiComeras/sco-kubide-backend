5aabf5aa2dbed238ccc4396d547ae3b5f49cac23
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-06-13 00:02:47 +0200

    ADD - Notification Schema

---
+++ src/modules/notifications/schema/notification.schema.ts
@@ -0,0 +1,26 @@
+import { Schema, Types } from 'mongoose';
+import { MONGODB_CONSTANTS } from 'src/modules/mongo-db/mongo-db.constants';
+import { INotification } from '../interface/inotification.interface';
+
+export const notificationSchema = new Schema<INotification>(
+  {
+    user: { 
+      type: Types.ObjectId, 
+      ref: MONGODB_CONSTANTS.USERS.MODEL, 
+      required: true
+    },
+    message: {
+      type: Types.ObjectId, 
+      ref: MONGODB_CONSTANTS.MESSAGES.MODEL, 
+      required: true,
+    },
+    opened: {
+      type: Boolean, 
+      required: false,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
