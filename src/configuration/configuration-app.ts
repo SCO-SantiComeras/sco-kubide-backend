587cfe2fd0cc0682652e21710dc96301f2c94a0b
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-06-12 20:10:13 +0200

    ADD - App host env variable

--- src/configuration/configuration-app.ts
+++ src/configuration/configuration-app.ts
@@ -3,5 +3,6 @@ import { registerAs } from '@nestjs/config';
 export const configurationApp = registerAs('app', () => ({
   env: process.env.ENV_APP,
   port: parseInt(process.env.PORT_APP, 10 || 3000),
+  host: process.env.HOST_APP,
   production: process.env.PRODUCTION_APP == 'true',
 }));
