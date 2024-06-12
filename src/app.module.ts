299094f1761c2feacbc686969b83b6933f728952
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-06-12 20:14:11 +0200

    ADD - Config Module & Configuration files import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MongoDbModule } from './modules/mongo-db/mongo-db.module';
+import { ConfigModule } from '@nestjs/config';
+import { configurationApp } from './configuration/configuration-app';
+import { configurationMongo } from './configuration/configuration-mongo';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [
+        configurationApp,
+        configurationMongo,
+      ],
+      envFilePath: `./env/${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
     MongoDbModule.register({
       ip: '127.0.0.1',
       port: 27017,
