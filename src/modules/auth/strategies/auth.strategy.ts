b03d2d9752f0979ea0f0ac5ef964dc4bf3013896
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-06-12 22:15:13 +0200

    ADD - Auth Jwt Strategy

---
+++ src/modules/auth/strategies/auth.strategy.ts
@@ -0,0 +1,31 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { IUser } from '../../users/interface/iuser.interface';
+import { UsersService } from '../../users/users.service';
+import { JwtPayload } from '../interface/jwt-payload.interface';
+import { HTTP_ERROR_CONSTANTS } from '../../../constants/http-error-messages.constants';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get('auth.secret'),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<IUser> {
+    const user = await this.usersService.findUserByEmail(payload.email);
+    if (!user) {
+      console.log(`[Jwt Strategy Validate] User '${payload.email} is not authorized`);
+      throw new HttpException(HTTP_ERROR_CONSTANTS.AUTH.UNAUTHORIZED, HttpStatus.UNAUTHORIZED);
+    }
+
+    return user;
+  }
+}
