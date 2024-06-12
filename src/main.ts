dc04b4650ccb1dcf75174cd0716efcbabccf1536
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-06-12 18:09:57 +0200

    ADD - Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  const swagger = new DocumentBuilder()
+    .setTitle('SCO - Kubide backend')
+    .setDescription('Documentación sobre endpoints de la prueba técnica Kubide Backend')
+    .setVersion('1.0')
+    .addTag('Kubide')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, swagger);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(3000).then(() => {
+    console.log(`[bootstrap] Swagger started in url 'http://localhost:3000/swagger'`);
+    console.log(`[bootstrap] App started in 'http://localhost:3000'`);
+  });
 }
 bootstrap();
