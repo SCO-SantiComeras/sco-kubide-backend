3c6ca0e3116a1f6a7b51ef366555ea6845e597e8
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-06-13 20:31:54 +0200

    Del Controller service

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BcryptService } from './bcrypt/bcrypt.service';
-import { ControllerService } from './controller/controller.service';
 
 @Module({
   providers: [
     BcryptService,
-    ControllerService,
   ],
   exports: [
     BcryptService,
-    ControllerService,
   ],
 })
 export class SharedModule {}
